13a37a9187eb9552ac6c87e4969ae43bc0476577
Murat-Kurbanov <46996771+Murat-Kurbanov@users.noreply.github.com>
2019-02-24 17:24:14 +0600

    Update axios-blog.js

--- src/axios-blog.js
+++ src/axios-blog.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://labwork-js3.firebaseio.com/'
+    baseURL: 'https://todolist-a70d4.firebaseio.com/'
 });
 
 export default instance;
